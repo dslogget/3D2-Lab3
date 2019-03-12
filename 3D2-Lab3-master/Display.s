807a5d3ef58b5934710fa5fc8143a2789ff10206
SenanS <30498489+SenanS@users.noreply.github.com>
2019-03-12 12:29:34 +0000

    Added [untested] body to App.s

---
+++ 3D2-Lab3-master/Display.s
@@ -0,0 +1,28 @@
+	AREA	DisplayCode, CODE, READONLY
+	IMPORT IO1DIR
+	IMPORT IO1CLR
+	IMPORT IO1SET
+		
+	; Display 4 LSbs the value of R1 to the display
+	EXPORT Display
+	
+Display
+	STMFD 	sp!, 	{R0, R2-R12, lr}				;Store all registers that don't return a value
+	TST R1, #2_1000
+	ORRNE R2, #2_0001
+	TST R1, #2_0100
+	ORRNE R2, #2_0010
+	TST R1, #2_0010
+	ORRNE R2, #2_0100
+	TST R1, #2_0001
+	ORRNE R2, #2_1000
+	LDR R0, =IO1SET
+	LDR R4, =(2_1111 << 16)
+	STR R4, [R0]
+	LDR R0, =IO1CLR
+	LSL R2, #16
+	STR R2, [R0]
+	LDMFD 	sp!, 	{R1-R12, pc}
+	
+	
+	END
